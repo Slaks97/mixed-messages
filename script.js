6ca75ea85dda5659d558e253fae3137a1fb256d5
Sophie <sophielaksimi@gmail.com>
2024-04-02 11:00:38 +0100

    Event listener added to button

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-// MIXED MESSAGES PROJECT
+let quotes = ["No pain, no gain", "Fail to prepare, prepare to fail", "Carpe Diem", "I can, I will"];
 
-let testingOutput = "It's working :)"
-console.log(testingOutput);
+const generateRandomQuote = () => {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    let randomQuote = quotes[randomIndex];
+    return randomQuote;
+};
+console.log(generateRandomQuote());
+
+let button = document.querySelector(".btn");
+let paragraph = document.querySelector(".para");
+let revealQuote = document.querySelector(".quote");
+
+button.classList.remove("hide");
+paragraph.classList.remove("hide");
+revealQuote.classList.add("hide");
+
+button.addEventListener("click", function() {
+    generateRandomQuote();
+    button.classList.add("hide");
+    paragraph.classList.add("hide");
+    revealQuote.classList.remove("hide");
+});
